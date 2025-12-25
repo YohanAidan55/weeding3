c51a175bedb08a20aefeec990ee4f596f7a3c3ce
Yohan Aidan <yohanaidan@MacBook-Pro-de-Yohan.local>
2025-12-25 17:30:41 +0100

    update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,20 @@
-import Component0 from "./component0.tsx";
-import {Fade, IconButton} from "@mui/material";
-import { useSelector, useDispatch } from 'react-redux';
-import {useRef, useState} from "react";
+import { IconButton } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { useRef, useState } from "react";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
 import backgroundMusic from "../assets/music.mp3";
 import { show } from "../appSlice.tsx";
-import Component5 from "./component5/component5.tsx";
+import Component0 from "./component0.tsx";
 import Component1 from "./component1/component1.tsx";
 import Component2 from "./component2/component2.tsx";
+import Component5 from "./component5/component5.tsx";
 import Component6 from "./component6/component6.tsx";
+import { motion, AnimatePresence } from "framer-motion";
 
 function HomePage() {
     const dispatch = useDispatch();
-    const showComponents = useSelector((state: { app: { showComponents: never; }; }) => state.app.showComponents);
+    const showComponents = useSelector((state: { app: { showComponents: boolean; }; }) => state.app.showComponents);
     const [isPlaying, setIsPlaying] = useState(false);
     const audioRef = useRef(new Audio(backgroundMusic));
 
@@ -33,28 +34,45 @@ function HomePage() {
     };
 
     return (
-        <div>
-                {showComponents && (
-                    <IconButton
-                        onClick={togglePlayPause}
-                        style={{position: 'fixed', top: 10, right: 10}}
+        <div style={{ position: 'relative', minHeight: '100vh' }}>
+            {/* Bouton play/pause */}
+            {showComponents && (
+                <IconButton
+                    onClick={togglePlayPause}
+                    style={{ position: 'fixed', top: 10, right: 10, zIndex: 1000 }}
+                >
+                    {isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
+                </IconButton>
+            )}
+
+            {/* Transition fluide entre Component0 et le reste */}
+            <AnimatePresence>
+                {!showComponents && (
+                    <motion.div
+                        key="component0"
+                        initial={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 1 }}
+                        style={{ position: 'absolute', width: '100%' }}
                     >
-                        {isPlaying ? <PauseIcon/> : <PlayArrowIcon/>}
-                    </IconButton>
+                        <Component0 onShowComponents={handleShowComponents} />
+                    </motion.div>
                 )}
-            <Fade in={!showComponents} mountOnEnter unmountOnExit timeout={1000}>
-                <div>
-                    <Component0 onShowComponents={handleShowComponents}/>
-                </div>
-            </Fade>
-                {showComponents && (
-                    <>
-                        <Component1/>
-                        <Component2/>
-                        <Component5/>
-                        <Component6/>
-                    </>
-                )}
+            </AnimatePresence>
+
+            {showComponents && (
+                <motion.div
+                    key="components"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1 }}
+                >
+                    <Component1 />
+                    <Component2 />
+                    <Component5 />
+                    <Component6 />
+                </motion.div>
+            )}
         </div>
     );
 }
